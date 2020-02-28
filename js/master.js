dac848f5aef1335ba7a0c600af6b63a7d92c5567
naresh22255 <naresh22255@gmail.com>
2020-02-28 13:16:20 +0100

    completed assignment

--- js/master.js
+++ js/master.js
@@ -4,39 +4,40 @@ let string;
 let literal;
 /*** Dont touch */
 
-
 // question 1
 
 string = "Hello";
 
 const name = "Bob";
-string = "Hello "+name;
+string = "Hello " + name;
 literal = `Hello ${name}`;
 
 answer(string, literal);
 
-
 // question 2
 
-string = "Multiline\n"
-    + "string";
+string = "Multiline\n" + "string";
 literal = `Multiline
 string`;
 
 answer(string, literal);
 
-
 // question 3
 
-string = "123 Fake street\n"
-    + "Hackney\n"
-    + "London\n"
-    + "United Kingdom\n";
-    + "SN1 7UP";
-literal = ``;
+string =
+  "123 Fake street\n" +
+  "Hackney\n" +
+  "London\n" +
+  "United Kingdom\n" +
+  "SN1 7UP";
+literal = `123 Fake street
+Hackney
+London
+United Kingdom
+SN1 7UP
+`;
 
 answer(string, literal);
-
 
 // question 4
 
@@ -44,177 +45,173 @@ answer(string, literal);
 
 const title = "Noroff";
 string = "<h1>" + title + "</h1>";
-literal = ``;
+literal = `<h1>${title}</h1>`;
 
 answer(string, literal);
-
 
 // question 5
 
-const paragraph = "In auctor lobortis lacus. Phasellus gravida semper nisi. Fusce egestas elit eget lorem.";
+const paragraph =
+  "In auctor lobortis lacus. Phasellus gravida semper nisi. Fusce egestas elit eget lorem.";
 string = "<p>" + paragraph + "</p>";
-literal = ``;
+literal = `<p>${paragraph}</p>`;
 
 answer(string, literal);
-
 
 // question 6
 
 const numberOfLikes = 10;
 string = "<div>Number of likes: " + numberOfLikes + "</div>";
 
-literal = ``;
+literal = `<div>Number of likes: ${numberOfLikes}</div>`;
 
 answer(string, literal);
-
 
 // question 7
 
 const car = {
-    model: "Fiat",
-    price: "500000",
+  model: "Fiat",
+  price: "500000"
 };
 string = "<p>" + car.model + " for sale</p>";
-literal = ``;
+literal = `<p>${car.model} for sale</p>`;
 
 answer(string, literal);
-
 
 // question 8
 
 string = "<p>" + car.model + " for sale for " + car.price + "</p>";
-literal = ``;
+literal = `<p>${car.model} for sale for ${car.price}</p> `;
 
 answer(string, literal);
-
 
 // question 9
 
 const user = {
-    firstName: "Bob",
-    lastName: "Svensson"
+  firstName: "Bob",
+  lastName: "Svensson"
 };
 string = "<p>Welcome back " + user.firstName + " " + user.lastName + "</p>";
-literal = ``;
+literal = `<p>Welcome back ${user.firstName} ${user.lastName}</p>`;
 
 answer(string, literal);
-
 
 // question 10
 
 const article = {
-    title: "Latest news",
-    intro: "Quisque malesuada placerat nisl. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Curabitur ullamcorper ultricies nisi. Quisque malesuada placerat nisl.",
-    author: "Bob Stevenson",
-}
-string = "<h1>" + article.title + "</h1>\n"
-    + "<div>Writtem by " + article.author + "</div>\n"
-    + "<p>" + article.intro + "</p>"
-literal = ``;
+  title: "Latest news",
+  intro:
+    "Quisque malesuada placerat nisl. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Curabitur ullamcorper ultricies nisi. Quisque malesuada placerat nisl.",
+  author: "Bob Stevenson"
+};
+string =
+  "<h1>" +
+  article.title +
+  "</h1>\n" +
+  "<div>Writtem by " +
+  article.author +
+  "</div>\n" +
+  "<p>" +
+  article.intro +
+  "</p>";
+literal = `<h1>${article.title}</h1>
+<div>Writtem by ${article.author}</div>
+<p>${article.intro}`;
 
 answer(string, literal);
-
 
 // question 11
 
 const width = 10;
 const height = 30;
-string = "<div>Dimensions: " + (width * height) + " meters squared</div>";
-literal = `<div>Dimensions: ${width * height} meters squared</div>`
+string = "<div>Dimensions: " + width * height + " meters squared</div>";
+literal = `<div>Dimensions: ${width * height} meters squared</div>`;
 
 answer(string, literal);
 
 const adults = 10;
 const children = 30;
 string = "<div>Total people: " + (adults + children) + "</div>";
-literal = ``;
+literal = `<div>Total people ${adults + children}</div>`;
 
 answer(string, literal);
-
 
 // question 12
 
 const money = 3000;
 const spent = 1000;
 string = "<div>Remaining money: " + (money - spent) + "</div>";
-literal = ``;
+literal = `<div>Remaining money: ${money - spent}</div>`;
 
 answer(string, literal);
-
 
 // question 13
 
 function getName() {
-    return "Bob";
+  return "Bob";
 }
 
 string = "<div>Name: " + getName() + "</div>";
-literal = `<div>Name: ${getName()}</div>`
+literal = `<div>Name: ${getName()}</div>`;
 
 answer(string, literal);
-
 
 // question 14
 
 function getDate() {
-    return "12/03/2021";
+  return "12/03/2021";
 }
 
 string = "<div>Date: " + getDate() + "</div>";
-literal = ``;
+literal = `<div>Date: ${getDate()}</div>`;
 
 answer(string, literal);
-
 
 // question 15
 
 function numberOfAdults() {
-    return 10;
+  return 10;
 }
 function numberOfChildren() {
-    return 30;
+  return 30;
 }
 
-string = "<div>Total people: " + (numberOfAdults() + numberOfChildren()) + "</div>";
+string =
+  "<div>Total people: " + (numberOfAdults() + numberOfChildren()) + "</div>";
 literal = `<div>Total people: ${numberOfAdults() + numberOfChildren()}</div>`;
 
 answer(string, literal);
 
-
 // question 16
 
 function getWidth() {
-    return 10;
+  return 10;
 }
 function getHeight() {
-    return 30;
+  return 30;
 }
 
 string = "<div>Size: " + (getWidth() + getHeight()) + " meters squared</div>";
-literal = ``;
+literal = `<div>Size: ${getWidth() + getHeight()} meters squared</div>`;
 
 answer(string, literal);
-
 
 // question 17
 
 function getPanelContent(title) {
-    return `<h2>${title}</h2>
+  return `<h2>${title}</h2>
     <p>This is a paragraph</p>`;
 }
 
 string = "<div>" + getPanelContent("Welcome") + "</div>";
-literal = ``;
+literal = ` <div> ${getPanelContent("Welcome")}</div>`;
 
 answer(string, literal);
-
-
-
 
 /***** DONT TOUCH THIS *****/
 
 function answer(string, literal) {
-    console.log("----- question " + questionNumber++ + " -----");
-    console.log("string:\n\"" + string + "\"");
-    console.log("literal:\n`" + literal + "`");
+  console.log("----- question " + questionNumber++ + " -----");
+  console.log('string:\n"' + string + '"');
+  console.log("literal:\n`" + literal + "`");
 }
